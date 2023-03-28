d2230de47eb2fb17a9086c08c1f1bfdd8f2240b6
Luis Alberto Suárez Gómez <billisuarezgomez@hotmail.com>
2023-03-28 15:51:25 -0600

    Se eliminó la detección de la carpeta target

--- src/main/java/com/lumtec/computo/ClipBoar.java
+++ src/main/java/com/lumtec/computo/ClipBoar.java
@@ -1,16 +1,9 @@
 
 package com.lumtec.computo;
 
-import java.awt.AWTException;
-import java.awt.HeadlessException;
-import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
-import java.awt.datatransfer.UnsupportedFlavorException;
-import java.awt.event.KeyEvent;
-import java.io.IOException;
 
 /**
  *
